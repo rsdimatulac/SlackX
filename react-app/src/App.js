82f020b3302aba59cfd0040241853531f929545b
Qingwei <qchen6060@gmail.com>
2021-05-22 23:43:59 -0400

    practice coding on splash page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,6 +8,7 @@ import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
 import { authenticate } from "./store/session";
+import SplashPage from "./components/SplashPage/Splash";
 
 function App() {
   const user = useSelector(state => state.session.user)
@@ -42,7 +43,7 @@ function App() {
           <User />
         </ProtectedRoute>
         <ProtectedRoute path="/" exact={true} >
-          <h1>My Home Page</h1>
+          <SplashPage />
         </ProtectedRoute>
       </Switch>
     </BrowserRouter>
