d0814edec9ac996a04b880f901a00f51e09a818e
Qingwei <qchen6060@gmail.com>
2021-05-24 00:06:52 -0400

    fix login/signup route

--- react-app/src/components/SplashPage/NavBar.js
+++ react-app/src/components/SplashPage/NavBar.js
@@ -6,11 +6,21 @@ import SlackXLogo from '../../imgs/SlackX-Logo.png';
 import "./NavBar.css";
 import SearchButton from '../../imgs/searchButton.png';
 import { CgChevronDown, CgSearch } from 'react-icons/cg';
+import { useHistory } from "react-router-dom";
 
 
 
 const NavBar = ({ isLoaded }) => {
+  const history = useHistory();
   const [logo, setLogo] = useState(SlackXLogo);
+
+  const toLoginPage = () => {
+    history.push(`/login`);
+  }
+
+  const toSignUp = () => {
+    history.push(`/sign-up`);
+  }
 
   return (
     <nav id = "navbar" className="NavbarContainer">
@@ -27,22 +37,22 @@ const NavBar = ({ isLoaded }) => {
 
         <h1 className="SlackX"> SlackX</h1>
         <div className="navBar-left">
-          <button>Product <CgChevronDown /></button>
+          <button style={{cursor: 'pointer'}}>Product <CgChevronDown /></button>
           <button>Enterprise</button>
           <button>Resources</button>
           <button>Pricing</button>
         </div>
         <div className="navBar-right">
           {/* <CgSearch /> */}
-          <button className="button1" to="/login" exact={true} >
+          <button className="button1" onClick={toLoginPage} exact={true} style={{cursor: 'pointer'}}>
             Login
           </button>
 
-          <button className="button1" to="/sign-up" exact={true}>
+          <button className="button1" onClick={toSignUp} exact={true} style={{cursor: 'pointer'}}>
             Sign Up
           </button>
-          <button className="button2">MEET THE TEAM</button>
-          <button className="button3">TRY FOR FREE</button>
+          <button className="button2" style={{cursor: 'pointer'}}>MEET THE TEAM</button>
+          <button className="button3" style={{cursor: 'pointer'}}>TRY FOR FREE</button>
 
           {/* <NavLink to="/users" exact={true} activeClassName="active">
             Users
