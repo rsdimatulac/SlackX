9998bc2fba417df8f82496ea0c497b72ef8762f5
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-05-24 10:25:15 -0500

    Added Channels Page draft

---
+++ react-app/src/components/SplashPage/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import LogoutButton from '../Auth/LogoutButton';
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/" exact={true} activeClassName="active">
+            Splash/Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" exact={true} activeClassName="active">
+            Login
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/sign-up" exact={true} activeClassName="active">
+            Sign Up
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/users" exact={true} activeClassName="active">
+            Users
+          </NavLink>
+        </li>
+        <li>
+          <LogoutButton />
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
---
+++ react-app/src/components/SplashPage/Splash.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from "./NavBar";
+
+const Splash = () => {
+    return (
+        <div>
+            <NavBar />
+            <h1>Splash Page</h1>
+        </div>
+    )
+}
+
+export default Splash;
