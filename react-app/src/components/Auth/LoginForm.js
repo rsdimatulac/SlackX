0e67219bae1991e34fa993b4518baeb329a2ba82
Qingwei <qchen6060@gmail.com>
2021-05-25 00:39:09 -0400

    EOD Monday

--- react-app/src/components/Auth/LoginForm.js
+++ react-app/src/components/Auth/LoginForm.js
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { login } from "../../store/session";
 import useConsumeContext from "../../context/FormModalContext";
+import slackLogo from "../../imgs/colorLogo.png";
+import "./LoginForm.css"
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -10,7 +12,7 @@ const LoginForm = () => {
   const [password, setPassword] = useState("");
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
-  const {handleSignUpModal} = useConsumeContext();
+  const { handleSignUpModal } = useConsumeContext();
 
   const onLogin = async (e) => {
     e.preventDefault();
@@ -31,38 +33,50 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error) => (
-          <div>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
-      </div>
-      <div>
-        Don't have an account yet? <strong onClick={handleSignUpModal} style={{ cursor: 'pointer' }}>Create an Account</strong>
-      </div>
-      {/* <button className="button3" onClick={loginDemoUser} type="submit" style={{ cursor: 'pointer' }}>TRY FOR FREE</button> */}
-    </form>
+    <div className="login__wrapper">
+      <form onSubmit={onLogin}>
+        <div className="login__header">
+          <h1>Login to </h1>
+          <img src={slackLogo} alt=""></img>
+          <h1> slackX </h1>
+        </div>
+        <div className="errors">
+          <div>・error1</div>
+          <div>・error2</div>
+          <div>・error3</div>
+
+          {errors.map((error) => (
+            <div key={error}>・{error}</div>
+          ))}
+        </div>
+        <div className="login__input">
+          <input
+            name="email"
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="login__input">
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="login__button">
+        <button style={{ cursor: 'pointer' }} type="submit">Login</button>
+        </div>
+        <div className="goto__signup">
+          <p>New to slackX?</p>
+          <h3 onClick={handleSignUpModal} style={{ cursor: 'pointer' }}>Create an account</h3>
+        </div>
+        {/* <button className="button3" onClick={loginDemoUser} type="submit" style={{ cursor: 'pointer' }}>TRY FOR FREE</button> */}
+      </form>
+    </div>
   );
 };
 
