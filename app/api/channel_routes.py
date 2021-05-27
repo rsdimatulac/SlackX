b4240ed5554eed8b91c9d8fc7c3e9693c755f131
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-05-27 15:11:05 -0500

    New channels created, rendering properly adn dynamic

--- app/api/channel_routes.py
+++ app/api/channel_routes.py
@@ -1,10 +1,20 @@
 from flask import Blueprint, jsonify, request
 from app.models import Channel, db
 from app.forms import ChannelForm
-from flask_login import current_user
+from flask_login import current_user, login_user
 
 channel_routes = Blueprint('channels', __name__)
 
+
+def channel_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:  # form.errors []
+        for error in validation_errors[field]:
+            errorMessages.append(f"{error}")
+    return errorMessages
 
 @channel_routes.route('/')  # GET /api/channels/
 def get_channels():
@@ -16,16 +26,6 @@ def get_channels():
 # current_user.channels.messages
 # @channel_routes.route('')
 
-def validation_errors_to_error_messages(validation_errors):
-    """
-    Simple function that turns the WTForms validation errors into a simple list
-    """
-    errorMessages = []
-    for field in validation_errors:  # form.errors []
-        for error in validation_errors[field]:
-            errorMessages.append(f"{error}")
-    return errorMessages
-
 # POST a new Channel
 @channel_routes.route('/', methods=['POST'])  # POST /api/channels/
 def post_channels():
@@ -33,15 +33,16 @@ def post_channels():
     Creates a new channel (public or private or dm)
     """
     form = ChannelForm()
+    print("FORM ERRORS", form.errors)
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit(): # if form is validated, store to DB
         channel = Channel(
             name = form.data['name'],
             channel_type = form.data['channel_type']
         )
+        current_user.channels.append(channel)
+
         db.session.add(channel)
         db.session.commit()
         return channel.to_dict()
-    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
-
-
+    return {'errors': channel_errors_to_error_messages(form.errors)}, 400
--- react-app/src/store/channel.js
+++ react-app/src/store/channel.js
@@ -15,45 +15,42 @@ const addChannel = (channel) => ({
 export const getChannels = () => async (dispatch) => {
     const res = await fetch("/api/channels/")
 
-    try{
-        if(!res.ok) throw res
+    try {
+        if (!res.ok) throw res
         const channels = await res.json()
         dispatch(setChannels(channels))
 
-    } catch(err) {
+    } catch (err) {
         console.log(err)
     }
 
 }
 
 export const createChannel = (name, channel_type) => async (dispatch) => {
-    try{
-        const res = await fetch("/api/channels/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({name, channel_type})
-        })
-        if(!res.ok) throw res
-        const channel = await res.json();
-        // console.log(channels)
-        if (channel.errors) {
-            return channel;
-        }
-        dispatch(addChannel(channel))
-        return {}
-    } catch(err) {
-        console.log(err)
+    const res = await fetch(`/api/channels/`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ name, channel_type })
+    })
+    const channel = await res.json();
+    if (channel.errors) {
+        return channel;
     }
+    dispatch(addChannel(channel))
+    return channel;
+
 }
 
 export default function channels(state = {}, action) {
-    switch(action.type) {
+    switch (action.type) {
         case GET_CHANNELS:
-            return {...state, ...action.channels}
+            return { ...state, ...action.channels }
         case ADD_CHANNEL:
-            return {...state, ...action.channel}
+            const newChannel = { ...state }
+            newChannel[action.channel.id] = action.channel
+            return newChannel;
         default:
             return state;
     }
