1486c028f254c552f4e89f7e46492d31b8ae1da7
Qingwei <qchen6060@gmail.com>
2021-05-28 16:49:47 -0400

    kimi-version2

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -48,6 +48,7 @@ const App = () => {
         <Route path="*">
           <NavBar />
           <ErrorPage />
+          <Footer />
         </Route>
       </Switch>
     </BrowserRouter>
--- react-app/src/components/404page/ErrorPage.js
+++ react-app/src/components/404page/ErrorPage.js
@@ -7,19 +7,24 @@ import "./ErrorPage.css";
 
 const ErrorPage = () =>{
 
-
+    useEffect(() => {
+        const navbarHeaders = document.querySelectorAll(".header__name");
+        navbarHeaders.forEach(header => {
+            header.style.display = "none";
+        })
+    }, [])
 
 
     return(
         <div className="ErrorPage">
             <div className="error_text">
-                <h1>WOOF! YOU FOUND KIMI!</h1>
-                <p>Unfortunately you have also found an elusive 404 page. You can go back, or stay with me and have a wonderful day! </p>
+                <h1>WOOF! </h1>
+                <h2>YOU FOUND KIMI!</h2>
+                <p>Unfortunately you have also found an elusive 404 page. <br/>You can go back, or stay with me and <br/>have a wonderful day! </p>
             </div>
             <div className="error_img">
                 <img id="kimi" src={kimi404}></img>
             </div>
-
         </div>
     )
 }
