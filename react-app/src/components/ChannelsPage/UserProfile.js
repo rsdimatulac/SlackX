63f23e6187b1027b06d9c9facfc759790c8c07e9
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-05-28 18:33:19 -0500

    Fixed 404 page. Added User Profile feature and styled

--- react-app/src/components/ChannelsPage/UserProfile.js
+++ react-app/src/components/ChannelsPage/UserProfile.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Modal } from "../../context/Modal";
+import { format } from "date-fns";
+import useConsumeContext from "../../context/FormModalContext";
+import "./UserProfile.css";
 
-const UserProfile = () => {
+const UserProfile = ({ user }) => {
+    const { handleProfileModal } = useConsumeContext();
     return (
-        <div>
-            <h1>User Profile Sidebar or Modal</h1>
-        </div>
+        <>
+        <Modal onClose={handleProfileModal}>
+            <h1>Profile</h1>
+            <div className="profile">
+                <div className="profile__avatar">
+                    <img src={user?.avatar} alt=""/>
+                </div>
+                <div className="profile__name">
+                    <h3>{user?.firstname} {user?.lastname}</h3>
+                    <p>{user?.bio}</p>
+                </div>
+                <hr/>
+                <div className="profile__about">
+                    <h4>Display name</h4>
+                    <p>{user?.firstname}</p>
+                    <h4>Joined since</h4>
+                        <p>{format(new Date(user?.created_at), "MMMM yyyy")}</p>
+                </div>
+            </div>
+        </Modal>
+        </>
     )
 }
 
