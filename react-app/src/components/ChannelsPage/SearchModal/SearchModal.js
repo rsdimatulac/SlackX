3e68ec5943a2f9436ad73c8bb552c56b0dbece0e
Qingwei <qchen6060@gmail.com>
2021-05-30 05:02:03 -0400

    finish the search bar function

--- react-app/src/components/ChannelsPage/SearchModal/SearchModal.js
+++ react-app/src/components/ChannelsPage/SearchModal/SearchModal.js
@@ -1,35 +1,43 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
+import { FaSearch as SearchIcon } from "react-icons/fa";
 import useConsumeContext from "../../../context/FormModalContext";
-import { Modal } from "../../../context/Modal";
+import { GrClose as Close } from "react-icons/gr";
+import "./SearchModal.css";
 
-
-
-const SearchModal = () => {
+const SearchModal = ({channels}) => {
     const { handleSearchModal } = useConsumeContext();
     const dispatch = useDispatch();
     const { userId } = useParams();
     const history = useHistory();
 
-    // const handleSubmit = async(e) => {
-    //     e.preventDefault();
-    //     history.push(`/users/${userId}/${channelId}`)
+    const handleRedirect = (channel_id) => {
+        handleSearchModal();
+        history.push(`/users/${userId}/${channel_id}`)
+    }
 
-    // }
-
+    const parseChannelName = (channel) => {
+        if (channel.channel_type === "dm") {
+            return  Object.values(channel.users).map(user => user.name).join(", ");
+        } else {
+            return channel.name;
+        }
+    }
 
     return (
-        <>
-            <Modal onClose={handleSearchModal}>
-                <div className="search__modal">
 
+        <div className="search__area">
+           <Close onClick={handleSearchModal} id = "search__area__close"/>
+           <ul>
+           {
+            channels?.map(channel => <li key={channel.id} style={{cursor: 'pointer'}} onClick={() => handleRedirect(channel.id)}># {parseChannelName(channel)} </li>)
+           }
+           </ul>
+        </div>
 
-
-
-                </div>
-            </Modal>
-        </>
     )
 
 }
+
+export default SearchModal;
