edf4d94299ca0f32ba5198f95e61872e133504e2
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-06-30 17:14:10 -0500

    Clean up

--- react-app/src/components/ChannelsPage/Sidebar/SidebarOptions.js
+++ react-app/src/components/ChannelsPage/Sidebar/SidebarOptions.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
+import React from 'react';
 import "./SidebarOptions.css";
 
 const SidebarOptions = ({ Icon, title, id, onClick }) => {
